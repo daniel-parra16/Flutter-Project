d19d387e2bc73ed636e6bfa34bcd3ee40bcc4dd4
daniel-parra16 <danielestebanparraruiz22@gmail.com>
2026-06-05 20:38:47 -0500

    login

---
+++ lib/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class login extends StatefulWidget {
+  const login({super.key});
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  String email = "";
+  String password = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
